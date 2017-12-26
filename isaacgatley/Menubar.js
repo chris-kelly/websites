763bf0a9ec7e4919bcdf12dd7fac7f0ef2e7c307
Chris Kelly <chris_kelly@live.co.uk>
2017-12-26 18:03:32 +0000

    isaacgatley.com

---
+++ isaacgatley/Menubar.js
@@ -0,0 +1,22 @@
+﻿		function openSubmenu() {
+		    var v = document.getElementById("submenu");
+			    if (v.className != "no_show")
+			       {v.className = "no_show"}
+			    else {v.className = "show"}
+			var w = document.getElementById("rotate1");
+			    if (w.className != "rotatesmall")
+			       {w.className = "rotatesmall"}
+			    else {w.className = "rotateoff"}
+		    var x = document.getElementById("rotate2");
+			    if (x.className != "rotatebig")
+			       {x.className = "rotatebig"}
+			    else {x.className = "rotateoff"}
+		    var y = document.getElementById("top");
+			    if (y.className != "not_visible")
+			       {y.className = "not_visible"}
+			    else {y.className = "visible"}
+		    var z = document.getElementById("bottom");
+			    if (z.className != "not_visible")
+			       {z.className = "not_visible"}
+			    else {z.className = "visible"}  
+			}
